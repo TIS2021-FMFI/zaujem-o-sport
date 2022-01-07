4c983330a7b92e95fe260520163d509b48f1d4e8
Jakub Mišovský <misovskyjakub@gmail.com>
2022-01-07 23:29:34 +0100

    Kostra stranky - nutne dorobit uz iba pages

--- frontend/src/pages/export/export.tsx
+++ frontend/src/pages/export/export.tsx
@@ -1,15 +1,20 @@
+import React from 'react';
 import {useState} from "react";
 import {useQuery} from "react-query";
 import {Spinner} from "react-bootstrap";
+import { CSVLink, CSVDownload } from "react-csv";
 
-export const Export = () => {
+const csvData = [
+    ["firstname", "lastname", "email"],
+    ["Ahmed", "Tomi", "ah@smthing.co.com"],
+    ["Raed", "Labes", "rl@smthing.co.com"],
+    ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+];
+<Link data={csvData}>Download me</Link>;
+<Download data={csvData} target="_blank" />;
 
-    return (<>
-        <header>
-            <h1>Export dát</h1>
-        </header>
-        <section>
 
-        </section>
-    </>)
-}
+
+
+
+
--- frontend/src/pages/uspesnost/uspesnost.tsx
+++ frontend/src/pages/uspesnost/uspesnost.tsx
@@ -38,7 +38,6 @@ export const Uspesnost = () => {
              */
         },
         onError: (error) => {
-
         }
     });
 
