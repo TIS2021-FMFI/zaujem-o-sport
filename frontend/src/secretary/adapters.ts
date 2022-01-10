42eb401789e7a41a4c40167a37a6b88890e9182f
MartysHD <martingergelhd@gmail.com>
2022-01-11 00:02:54 +0100

    Abstracted login/logout component for admin&secretary. JWT token authentication support added for the admin.

--- frontend/src/secretary/adapters.ts
+++ frontend/src/secretary/adapters.ts
@@ -3,22 +3,6 @@
 import {axiosProvider as axios} from "app/axios_provider";
 import {AxiosResponse} from "axios";
 
-export interface ApiSecretaryLoginResponseType {
-	message: string,
-	data: {
-		accessToken?: string
-	}
-}
-
-export const apiSecretaryLogin = (email: string, password: string)
-	: Promise<AxiosResponse<ApiSecretaryLoginResponseType>> =>
-{
-	return axios.post("/secretary/login", {
-		"email": email,
-		"password": password
-	});
-}
-
 export interface ApiListSportsType {
 	message: string,
 	data: {
