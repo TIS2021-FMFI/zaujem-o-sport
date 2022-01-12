7f54473a7da4a9dd2c15965599d466960acfc404
Sabinka <ssamporova@gmail.com>
2022-01-12 19:31:45 +0100

    add types, small fixes

--- backend/data-computation/__init__.py
+++ backend/data-computation/__init__.py
@@ -10,11 +10,11 @@ class Computations:
         # TODO
         # small db mirroring model will be initialize here
         # local info from db which are frequently used in computations are stored in lists, maps
-        PV1 = 0.5
-        PV2 = 0.16
-        PV3 = 0.34
-        PV4 = 2
-        DUMMY = 0.1  # to avoid division by zero error
+        self.PV1 = 0.5
+        self.PV2 = 0.16
+        self.PV3 = 0.34
+        self.PV4 = 2
+        self.DUMMY = 0.1  # to avoid division by zero error
 
     def allCountryIds(self):
         return [] # TODO
@@ -25,42 +25,42 @@ class Computations:
     def allSportIds(self):
         return []
 
-    def importance(self, countryK : id, sportN : id):
+    def importance(self, countryK : id, sportN : id) -> float:
         suma = 0
         for countryJ in self.allCountryIds():
             suma += self.sport_importance_in_country(sportN, countryJ) * self.norm_interconnectness(countryK, countryJ)
 
         return self.PV3 * self.norm_BGS(sportN) + (1 - self.PV3) * suma
 
-    def norm_interconnectness(self, countryK : id, countryJ : id):
+    def norm_interconnectness(self, countryK : id, countryJ : id) -> float:
 
         if countryK == countryJ:
             raise CompError("cannot compute norm interconectness for same countries")
 
         return self.total_interconnectness(countryK, countryJ) * (1 - self.econ_interconnectness(countryK, countryJ))
 
-    def total_interconnectness(self, countryK : id, countryJ : id):
+    def total_interconnectness(self, countryK : id, countryJ : id) -> float:
 
         if countryK == countryJ:
             raise CompError("cannot compute total interconectness for same countries")
 
         return (self.econ_interconnectness(countryK, countryJ) + self.nonecon_interconnectness(countryK, countryJ)) / 2
 
-    def econ_interconnectness(self, countryK : id, countryJ : id):
+    def econ_interconnectness(self, countryK : id, countryJ : id) -> float:
 
         if countryK == countryJ:
             raise CompError("cannot compute econ interconectness for same countries")
 
         return self.DUMMY
 
-    def nonecon_interconnectness(self, countryK : id, countryJ : id):
+    def nonecon_interconnectness(self, countryK : id, countryJ : id) -> float:
 
         if countryK == countryJ:
             raise CompError("cannot compute nonecon interconectness for same countries")
 
         return self.DUMMY
 
-    def sport_importance_in_country(self, sportN : id, countryK : id):
+    def sport_importance_in_country(self, sportN : id, countryK : id) -> float:
 
         normFun = self.norm_funding(sportN, countryK)
         normSuc = self.norm_success(sportN, countryK)
@@ -70,29 +70,29 @@ class Computations:
         else:
             return normSuc
 
-    def norm_funding(self, sportN : id, countryK : id):
+    def norm_funding(self, sportN : id, countryK : id) -> float:
 
         return self.DUMMY
 
 
-    def branch_funding(self, countryK : id, sportN : id, branchB : id):
+    def branch_funding(self, countryK : id, sportN : id, branchB : id) -> float:
         return self.DUMMY
 
 
-    def combi_branch_funding(self, countryK : id, combiQ : id):
+    def combi_branch_funding(self, countryK : id, combiQ : id) -> float:
         return self.DUMMY
 
 
-    def total_country_funding(self, countryK : id):
+    def total_country_funding(self, countryK : id) -> float:
         return self.DUMMY
 
 
-    def norm_success(self, sportN : id, countryK : id):
+    def norm_success(self, sportN : id, countryK : id) -> float:
 
         return self.success(sportN, countryK) / self.total_success(countryK)
 
 
-    def total_success(self, countryK : id):
+    def total_success(self, countryK : id) -> float:
 
         suma = 0
 
@@ -102,7 +102,7 @@ class Computations:
         return suma
 
 
-    def success(self, sportN : id, countryK : id):
+    def success(self, sportN : id, countryK : id) -> float:
 
         res = 0
 
@@ -124,34 +124,36 @@ class Computations:
 
         return res
 
-    def order(self, countryK : id, sportN : id):
+    def order(self, countryK : id, sportN : id) -> float:
         return self.DUMMY
 
-    def points(self, countryK : id, sportN : id):
-        return self.DUMMY
-
-
-    def max_points(self, sportN : id):
+    def points(self, countryK : id, sportN : id) -> float:
         return self.DUMMY
 
 
-    def num_countries_in_sport(self, sportN : id):
+    def max_points(self, sportN : id) -> float:
         return self.DUMMY
 
 
-    def total_points(self, countryK : id):
+    def num_countries_in_sport(self, sportN : id) -> float:
         return self.DUMMY
 
 
-    def min_order(self, countryK : id):
+    def total_points(self, countryK : id) -> float:
         return self.DUMMY
 
 
-    def norm_BGS(self, sportN : id):
+    def min_order(self, countryK : id) -> float:
+        return self.DUMMY
+
+
+    def norm_BGS(self, sportN : id) -> float:
 
         return self.BGS(sportN) / self.total_BGS()
 
-    def total_BGS(self):
+    def total_BGS(self) -> float:
+
+        # return self.DUMMY # -> this one is suming to 0, to test without errors, use this dummy constant
 
         suma = 0
 
@@ -160,9 +162,9 @@ class Computations:
 
         return suma
 
-    def BGS(self, sportN : id):
+    def BGS(self, sportN : id) -> float:
         return self.DUMMY
 
 
 c = Computations()
-print(c.importance(0,0))
+#print(c.importance(0,0))
