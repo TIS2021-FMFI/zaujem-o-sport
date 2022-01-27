4f5079ad53f63008df09d205496cc1f0b246aafa
MartysHD <martingergelhd@gmail.com>
2022-01-28 00:00:26 +0100

    Fetching, caching and notifing about progress of countries moved to custom hook.

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,6 +1,43 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import {useEffect, useState} from "react";
+import {useQuery} from "react-query";
+import {apiListCountries} from "../secretary/adapters";
+import createSnackbar, {dismissSnackbar, resolveSnackbar, SnackTypes} from "../components/snackbar/Snackbar";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useCountries = (): { isLoading: boolean, countries: string[][] } => {
+
+	const toastId = "countries_fetching";
+
+	const [countries, setCountries] = useState<string[][]>([]);
+
+	const {isLoading, data: response} = useQuery("list_countries", apiListCountries, {
+		onSuccess: (successResponse) => {
+			if (response === undefined)
+				resolveSnackbar(toastId, "Dáta úspešne načítané.");
+		},
+		onError: (error) => {
+			console.log(error);
+			if (response === undefined)
+				resolveSnackbar(toastId, "Dáta nebolo možné načítať.", false);
+		}
+	});
+
+	useEffect(() => {
+		if (response !== undefined)
+			setCountries(response.data.countries.map((country) => [country.name, country.code]));
+	}, [response]);
+
+	useEffect(() => {
+		if (isLoading)
+			createSnackbar("Načítavanie krajín...", SnackTypes.loading, false, toastId)
+	}, [isLoading]);
+
+	useEffect(() => { return () => dismissSnackbar(toastId) }, []);
+
+	return { isLoading, countries }
+}
--- frontend/src/secretary/pages/countries/Countries.tsx
+++ frontend/src/secretary/pages/countries/Countries.tsx
@@ -1,43 +1,11 @@
-import {useEffect, useState} from "react";
-import {useQuery} from "react-query";
-import {apiListCountries, countryType} from "secretary/adapters";
-import createSnackbar, {dismissSnackbar, resolveSnackbar, SnackTypes} from "components/snackbar/Snackbar";
 import {Table} from "components/table/Table";
-import {Col, Row} from "react-bootstrap";
-import {CenteredRow} from "../../../components/basic/CenteredRow";
+import {CenteredRow} from "components/basic/CenteredRow";
 import {CSVLink} from "react-csv";
+import {useCountries} from "app/hooks";
 
 export const Countries = () => {
 
-	const toastId = "countries_fetching";
-
-	const [countries, setCountries] = useState<string[][]>([]);
-
-	// TODO: maybe custom hook: useQuery/mutation with snackbar
-
-	const {isLoading, data: response} = useQuery("list_countries", apiListCountries, {
-		onSuccess: (successResponse) => {
-			if (response === undefined)
-				resolveSnackbar(toastId, "Dáta úspešne načítané.");
-		},
-		onError: (error) => {
-			console.log(error);
-			if (response === undefined)
-				resolveSnackbar(toastId, "Dáta nebolo možné načítať.", false);
-		}
-	});
-
-	useEffect(() => {
-		if (response !== undefined)
-			setCountries(response.data.countries.map((country) => [country.name, country.code]));
-	}, [response]);
-
-	useEffect(() => {
-		if (isLoading)
-			createSnackbar("Načítavanie krajín...", SnackTypes.loading, false, toastId)
-	}, [isLoading]);
-
-	useEffect(() => { return () => dismissSnackbar(toastId) }, []);
+	const { isLoading, countries } = useCountries();
 
 	return (<>
 		<CenteredRow as="header">
