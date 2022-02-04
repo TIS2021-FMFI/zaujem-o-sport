7076f125522c043539920d1b2ccc659e0a0f961b
Sabinka <ssamporova@gmail.com>
2022-02-04 21:09:09 +0100

    prepare csv parser for funding data in ADMIN

--- backend/admin/endpoints/upload/__init__.py
+++ backend/admin/endpoints/upload/__init__.py
@@ -1,8 +1,11 @@
+import json
+
 from flask import request
 from flasgger import SwaggerView
 from verification.jwt import is_admin
 from openpyxl import load_workbook
 from io import BytesIO
+import csv_parsers.csvParser as parser
 from csv_parsers.excelParser import excelParser
 from settings import DB
 import time
@@ -14,6 +17,7 @@ class UploadView(SwaggerView):
 
 		if len(request.files) == 0:
 			return {"message": "Missing uploaded file."}, 400
+
 
 		fundingFile = request.files.get("fundingFile")
 		successFile = request.files.get("successFile")
@@ -53,5 +57,33 @@ class UploadView(SwaggerView):
 					table.save()
 
 			# TODO: list of unknown sports in parsed[1]
+
+		if fundingFile:
+
+			requestJSON = json.loads(request.form["json"])
+			correction = requestJSON.get("correction")
+			countryCode = requestJSON.get("countryCode")
+			currency = requestJSON.get("currency")
+
+			if correction is None:
+				return {"message": "Missing required parameter: `correction`.", "data": {}}, 400
+			if not countryCode:
+				return {"message": "Missing required parameter: `countryCode`.", "data": {}}, 400
+			if not currency:
+				return {"message": "Missing required parameter: `currency`.", "data": {}}, 400
+
+			file = fundingFile
+			lines = []
+			for line in file:
+				lines.append(line.decode("utf-8").strip())
+
+			p = parser.csvParser()
+			suggestions = p.findFailures(lines, correction, countryCode, currency)
+
+			if len(suggestions) == 0:
+				p.saveResults()
+				return {"message": "ok"}
+			else:
+				return {"message": "fail", "suggestions": suggestions}, 400
 
 		return {}
